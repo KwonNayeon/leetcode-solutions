a3ab09e2c387cef3990fe0a3906c13f9b497aca1
Nayeon <nayeon.k.datacareer@gmail.com>
2025-04-24 16:41:00 +0900

    Time: 0 ms (100%), Space: 17.9 MB (64.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-
-        s = s.replace(' ', '').lower()
-        t = t.replace(' ', '').lower()
-
-        if sorted(s) == sorted(t):
-            return True
-        else:
-            return False
+        
+        return Counter(s) == Counter(t)
         
