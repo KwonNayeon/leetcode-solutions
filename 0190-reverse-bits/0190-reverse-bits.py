1bcd49d26cdd6f27b8ec854c673cf0da4bb873e4
Nayeon <nayeon.k.datacareer@gmail.com>
2025-05-19 16:03:55 -0400

    Time: 49 ms (7.28%), Space: 18 MB (15.31%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,8 +1,8 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-
-        binary = format(n, '032b')
-
-        reversed_binary = binary[::-1]
-        
-        return int(reversed_binary, 2)
+        result = 0
+        for i in range(32):
+            result <<= 1
+            result |= n & 1
+            n >>= 1
+        return result
