1fb3ee5a87f778c2d6350171feea770be3074894
Nayeon <nayeon.k.datacareer@gmail.com>
2025-05-29 14:25:17 -0400

    Time: 271 ms (64.98%), Space: 67.5 MB (46.79%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
@@ -0,0 +1,9 @@
+# df.drop_duplicates(subset=['name'])
+import pandas as pd
+
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+
+    person.sort_values(by=["id"], inplace=True)
+
+    person.drop_duplicates(subset=["email"], keep="first", inplace=True)
+    
