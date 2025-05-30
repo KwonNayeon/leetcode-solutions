39b70b673eda55dc9ab76661064fafa5a803c646
Nayeon <nayeon.k.datacareer@gmail.com>
2025-05-29 21:06:09 -0400

    Time: 19 ms (98.56%), Space: 19.6 MB (17.75%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,23 +1,21 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        if not heights:
-            return []
-        
         rows, cols = len(heights), len(heights[0])
+
         pacific = set()
         atlantic = set()
 
         def dfs(r, c, visited):
             visited.add((r, c))
-            directions = [(1,0), (-1,0), (0,1), (0,-1)]
 
-            for dr, dc in directions:
+            for dr, dc in [(0, 1), (-1, 0), (0, -1), (1, 0)]:
                 new_r, new_c = r + dr, c + dc
-                if (new_r < 0 or new_r >= rows or
-                    new_c < 0 or new_c >= cols or
-                    (new_r, new_c) in visited):
-                    continue
-                if heights[new_r][new_c] >= heights[r][c]:
+
+                if (0 <= new_r < rows and
+                    0 <= new_c < cols and
+                    (new_r, new_c) not in visited and
+                    heights[new_r][new_c] >= heights[r][c]):
+
                     dfs(new_r, new_c, visited)
 
         for c in range(cols):
@@ -30,4 +28,4 @@ class Solution:
         for r in range(rows):
             dfs(r, cols-1, atlantic)
 
-        return list(pacific & atlantic)
+        return [[r, c] for r, c in pacific & atlantic]
