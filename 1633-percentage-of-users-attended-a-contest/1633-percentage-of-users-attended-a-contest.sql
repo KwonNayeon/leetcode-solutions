7a93c0184bbcd87455aa176f6e18005ec692825f
Nayeon <nayeon.k.datacareer@gmail.com>
2025-06-05 21:51:34 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+select
+    r.contest_id,
+    round(count(u.user_id) * 100.0 / count(*), 2) as percentage
+from Register r
+left join Users u
+on r.user_id = u.user_id
+group by r.contest_id
+order by percentage desc, contest_id asc
+;
