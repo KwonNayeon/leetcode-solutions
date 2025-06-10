1d3ca71bb1881ace519bacbf3984ce2529b13cce
Nayeon <nayeon.k.datacareer@gmail.com>
2025-06-09 23:27:29 -0400

    Time: 301 ms (7.81%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -3,6 +3,6 @@ select
     query_name,
     round(avg(rating * 1.0 / position), 2) as quality,
     round(count(case when rating < 3 then 1 end) * 100.0 / count(*), 2) as poor_query_percentage
-from Queries
+from queries
 group by query_name
 ;
