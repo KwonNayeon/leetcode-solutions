4bb40059e01307c26f4a6107d0a711d4609e5e37
Nayeon <nayeon.k.datacareer@gmail.com>
2025-07-16 16:55:47 -0400

    Time: 304 ms (74.66%), Space: 67.8 MB (91.75%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.py
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.py
@@ -3,10 +3,10 @@ import pandas as pd
 def user_activity(activity: pd.DataFrame) -> pd.DataFrame:
     df = activity
     df_filtered = df[
-        (df['activity_date'] >= '2019-06-28') &
-        (df['activity_date'] < '2019-07-28') &
-        (df['activity_type'].notna())
-        ]
+                    (df['activity_date'] >= '2019-06-28') &
+                    (df['activity_date'] < '2019-07-28') &
+                    (df['activity_type'].notna())
+                    ]
 
     result = df_filtered.groupby('activity_date')['user_id'].nunique().reset_index()
     result.columns = ['day', 'active_users']
