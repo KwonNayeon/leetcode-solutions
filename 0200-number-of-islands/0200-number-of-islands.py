05d1eb7d78ebe678d199878dc7c2b511b893fd11
Nayeon <nayeon.k.datacareer@gmail.com>
2025-08-25 14:46:13 -0400

    Time: 254 ms (46.64%), Space: 20.4 MB (43.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,13 +1,7 @@
-from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
         n, m = len(grid), len(grid[0])
-        
-        islands = 0
-
         visited = [[False] * m for _ in range(n)]
-
         dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
         def in_range(r, c):
@@ -16,24 +10,23 @@ class Solution:
         def passable(r, c):
             return grid[r][c] == "1"
 
-        def bfs(r, c):
-            q = deque([(r, c)])
+        def dfs_recursive(r, c):
             visited[r][c] = True
 
-            while q:
-                cur_r, cur_c = q.popleft()
+            for dr, dc in dirs:
+                nr = r + dr
+                nc = c + dc
 
-                for dr, dc in dirs:
-                    nr, nc = cur_r + dr, cur_c + dc
-                    if in_range(nr, nc) and passable(nr, nc) and not visited[nr][nc]:
-                        visited[nr][nc] = True
-                        q.append((nr, nc))
+                if (in_range(nr, nc) and passable(nr, nc) and not visited[nr][nc]):
+                    dfs_recursive(nr, nc)
+
+        islands = 0
 
         for r in range(n):
             for c in range(m):
                 if grid[r][c] == "1" and not visited[r][c]:
+                    dfs_recursive(r, c)
                     islands += 1
-                    bfs(r, c)
 
         return islands
         
