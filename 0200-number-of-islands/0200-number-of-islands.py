f6cfb0aa7f3aae98007004d24ad2a63bffbc4852
Nayeon <nayeon.k.datacareer@gmail.com>
2025-08-24 21:00:06 -0400

    Time: 258 ms (42.12%), Space: 20.3 MB (43.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,39 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+
+        n, m = len(grid), len(grid[0])
+        
         islands = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    self.visit_island(grid, i, j)
+
+        visited = [[False] * m for _ in range(n)]
+
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        def in_range(r, c):
+            return 0 <= r < n and 0 <= c < m
+
+        def passable(r, c):
+            return grid[r][c] == "1"
+
+        def bfs(r, c):
+            q = deque([(r, c)])
+            visited[r][c] = True
+
+            while q:
+                cur_r, cur_c = q.popleft()
+
+                for dr, dc in dirs:
+                    nr, nc = cur_r + dr, cur_c + dc
+                    if in_range(nr, nc) and passable(nr, nc) and not visited[nr][nc]:
+                        visited[nr][nc] = True
+                        q.append((nr, nc))
+
+        for r in range(n):
+            for c in range(m):
+                if grid[r][c] == "1" and not visited[r][c]:
                     islands += 1
+                    bfs(r, c)
+
         return islands
-    
-    def visit_island(self, grid, i, j):
-        if (i < 0 or i >= len(grid) or 
-            j < 0 or j >= len(grid[0]) or 
-            grid[i][j] != '1'):
-            return
-        
-        grid[i][j] = '0'
-        
-        self.visit_island(grid, i+1, j)  # 위
-        self.visit_island(grid, i-1, j)  # 아래
-        self.visit_island(grid, i, j+1)  # 오른쪽
-        self.visit_island(grid, i, j-1)  # 왼쪽
         
